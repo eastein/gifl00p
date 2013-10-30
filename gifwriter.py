82f03ccafb94c3f039dcbedadabf38351f85c4b6
Eric Stein <toba@des.truct.org>
2013-10-30 02:18:27 -0500

    note about usage of convert

--- gifwriter.py
+++ gifwriter.py
@@ -5,6 +5,7 @@ class Error(Exception) :
 
 def write_gif(files, fps, output_file) :
 	# include +dither?
+	# or +remap or -remap? read http://www.imagemagick.org/Usage/quantize/#remap_common
 	cmd = ['convert', '-delay', '1x%d' % fps] + files + ['-coalesce', '-layers', 'OptimizeTransparency', output_file]
 	if subprocess.call(cmd) != 0 :
 		raise Error
